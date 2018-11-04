8067a32b42bcd2ad41e4b20ec3e21b455257f6ea
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-11-04 23:31:19 +0600

    ideal condition

---
+++ CSE322_Offline1_Problem1/src/SMTP_Client/SMTP_Client.java
@@ -0,0 +1,70 @@
+package SMTP_Client;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class SMTP_Client {
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        String mailServer = "webmail.buet.ac.bd";
+        //String mailServer = "localhost";
+
+        InetAddress mailHost = InetAddress.getByName(mailServer);
+        InetAddress localHost = InetAddress.getLocalHost();
+
+        Socket smtpSocket = new Socket(mailHost,25);
+
+        BufferedReader in =  new BufferedReader(new InputStreamReader(smtpSocket.getInputStream()));
+        PrintWriter pr = new PrintWriter(smtpSocket.getOutputStream(),true);
+
+        String initialID = in.readLine();
+        System.out.println(initialID);
+
+        pr.println("HELO "+localHost.getHostName());
+        pr.flush();
+        String welcome = in.readLine();
+        System.out.println(welcome);
+
+        pr.println("MAIL FROM:ishtiaq_niloy@yahoo.com");
+        pr.flush();
+        welcome = in.readLine();
+        System.out.println(welcome);
+
+
+        pr.println("RCPT TO:ishtiaq.niloy@gmail.com");
+        pr.flush();
+        welcome = in.readLine();
+        System.out.println(welcome);
+
+
+        pr.println("DATA");
+        pr.println("Subject: Sample");
+        pr.println("From: abcd@gmail.com");
+        pr.println("To: ishtiaq.niloy@gmail.com");
+        pr.println("");
+
+        pr.println("Greetings,");
+        pr.println("Bye");
+
+        pr.println(".");
+        pr.flush();
+
+        welcome = in.readLine();
+        System.out.println(welcome);
+
+        pr.println("QUIT");
+        pr.flush();
+
+        welcome = in.readLine();
+        System.out.println(welcome);
+
+        // TODO code application logic here
+    }
+}
