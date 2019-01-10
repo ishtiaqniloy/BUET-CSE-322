d80f0b37cf9b7c7dc065607d668731a6327cc1cc
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-01-10 20:58:51 +0600

    Project creation

---
+++ RDT/cmake-build-debug/CMakeFiles/RDT.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Drive/L-3 T-2/CSE 322 - Computer Networks Sessional/Assignment 4 - RDT/CSE_322_Assignment4_RDT/RDT/main.c" "D:/Drive/L-3 T-2/CSE 322 - Computer Networks Sessional/Assignment 4 - RDT/CSE_322_Assignment4_RDT/RDT/cmake-build-debug/CMakeFiles/RDT.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RDT/cmake-build-debug/CMakeFiles/RDT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RDT.dir/main.c.obj"
+  "RDT.pdb"
+  "RDT.exe"
+  "RDT.exe.manifest"
+  "libRDT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RDT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
