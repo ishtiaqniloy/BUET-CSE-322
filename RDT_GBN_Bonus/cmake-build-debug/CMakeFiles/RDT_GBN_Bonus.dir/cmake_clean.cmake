0ae8c2e444990e2b00f4777466b4a989a4b46370
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-01-27 03:03:05 +0600

    cumulative ack done

---
+++ RDT_GBN_Bonus/cmake-build-debug/CMakeFiles/RDT_GBN_Bonus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RDT_GBN_Bonus.dir/main.cpp.obj"
+  "RDT_GBN_Bonus.pdb"
+  "RDT_GBN_Bonus.exe"
+  "RDT_GBN_Bonus.exe.manifest"
+  "libRDT_GBN_Bonus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RDT_GBN_Bonus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
